15ffc6f12d80a019a733eef59066e3e5cbeaf473
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-22 05:54:43 +0300

    Updating city storage mechanism

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ Represents a city for MySQL database
+
+    Inherits from SQLAlchemy Base and links to table cities
+
+    Attributes:
+            __tablename__(str): Name of table to store data
+            name(string): Name of city
+            state_id(String): State id of the city
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
