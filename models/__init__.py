63e8597cd776ef2c144eed445292d21d7943aa4c
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-27 00:01:48 +0300

    Changing instatiation using environment variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates an object of class FileStorage
+
+If the environment variable 'HBNB_TYPE_STORAGE' is set to 'db',
+instatiate a database storage engine (DBStorage)
+
+Otherwise, instatiate a filestorage engine
+"""
+from os import getenv
 
 
-storage = FileStorage()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
