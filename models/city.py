676961ec47a0528501ecb25574fcb26d4fa05619
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-29 22:01:34 +0300

    Adding access using database

--- models/city.py
+++ models/city.py
@@ -17,7 +17,15 @@ class City(BaseModel, Base):
             name(string): Name of city
             state_id(String): State id of the city
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+    if models.storage_t == "db":
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes city"""
+        super().__init__(*args, **kwargs)
