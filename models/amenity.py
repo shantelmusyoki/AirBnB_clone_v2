6b37425c35c0c6f10c96b10d27d8be907e64ea46
JaminBenjamins <jaminbenjamins@gmail.com>
2023-06-29 22:16:55 +0300

    Adding connection to database and other columns

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import Base
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represent an Amenity for a MySQL database.
+    Inherits from SQLAlchemy and links to MySQL table amenities
+
+    Attributes:
+        __tablename__(str): The name of MySQL table to store Amenities
+        name(str): Amenity name
+        place_amenities(relationship): Place-Amenity Relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                    viewonly=False)
